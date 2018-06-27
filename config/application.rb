1979a55df929a0e90f7932ef4fabac1c34a10d66
Quinn Stearns <quinn.stearns@liveramp.com>
2018-06-27 09:03:16 -0700

    Add Rails Logger and Appender

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module LoggingProj
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.semantic_logger.add_appender(
+      file_name: 'log/a_json_log.json',
+      formatter: :json
+    )
   end
 end
